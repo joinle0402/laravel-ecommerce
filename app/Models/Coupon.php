da59fc9351aeb2e5bee6d4ba3a981c766d113616
John Smith <nhoxhero073@gmail.com>
2023-05-11 18:04:45 +0700

    Setup tables

---
+++ app/Models/Coupon.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Coupon extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'type',
+        'value',
+        'expiry_date',
+    ];
+}
---
+++ app/Models/ProductDetail.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductDetail extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'product_id',
+        'size',
+        'color',
+        'quantity',
+    ];
+}
